7d35ba465d9aae408a92d7851ec823a65f30a44e
ypablo <yfedora@gmail.com>
2019-09-11 22:34:22 +0100

    Add input field

--- src/App.js
+++ src/App.js
@@ -35,11 +35,16 @@ class App extends Component {
   }
 
   clearList = () => {
-    console.log("clear list")
+    this.setState({
+      items: []
+    })
   }
 
   handleDelete = (id) => {
-    console.log(`handle delete ${id}`)
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    })
   }
 
   handleEdit = (id) => {
