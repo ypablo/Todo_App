482e0d60f8f3f6bd9d85ca8337411ac3610a800f
ypablo <yfedora@gmail.com>
2019-09-11 15:48:36 +0100

    Add input field

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {
+    items: [{ id: 1, title: "Buy shortbread" }, { id: 2, title: "SQL task" }],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = (e) => {
+    console.log("handle change")
+  }
+
+  handleSubmit = (e) => {
+    console.log("handle submit")
+  }
+
+  clearList = () => {
+    console.log("clear list")
+  }
+
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`)
+  }
+
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`)
+  }
+
+  render() {
+    return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.state.editItem} />
+            <TodoList item={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+        const { item, handleChange, handleSubmit, editItem } = this.props;
         return (
-            <div>
-                <h1>Hello from Todo Input</h1>
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className="fas fa-book" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
