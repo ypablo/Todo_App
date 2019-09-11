2cd49a2c14fd849fc39523116cc149dda16814ab
ypablo <yfedora@gmail.com>
2019-09-11 23:58:18 +0100

    Add edit capability

--- src/App.js
+++ src/App.js
@@ -48,7 +48,14 @@ class App extends Component {
   }
 
   handleEdit = (id) => {
-    console.log(`handle edit ${id}`)
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    })
   }
 
   render() {
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -12,9 +12,14 @@ export default class TodoInput extends Component {
                                 <i className="fas fa-book" />
                             </div>
                         </div>
-                        <input type="text" className="form-control text-capitalize" placeholder="add todo item" value={item} onChange={handleChange}/>
+                        <input type="text" className="form-control text-capitalize" placeholder="add todo item" value={item} onChange={handleChange} />
                     </div>
-                    <button type="submit" className="btn btn-block btn-primary mt-3 text-uppercase">Add item</button>
+                    <button 
+                    type="submit"
+                    disabled={item?false:true} 
+                    className={editItem ? "btn btn-block btn-success mt-3" : "btn btn-block btn-primary mt-3 text-uppercase"}> 
+                    {editItem? "edit item" : "add item" }
+                    </button>
                 </form>
             </div>
         )
